5d3c37365558d4875602e46e32f3264dbbc84a1a
Nicolas St-Laurent <stlaurent.nicolas@gmail.com>
2022-08-09 11:32:24 -0400

    Fix sudoku module. Remove example call.

--- 01/sudoku.rb
+++ 01/sudoku.rb
@@ -1,20 +1,5 @@
 module Sudoku
 
-  class Invalid < StandardError
-  end
-
-  class Impossible < StandardError
-  end
-
-  BoxOfIndex = [
-    0,0,0,1,1,1,2,2,2,0,0,0,1,1,1,2,2,2,0,0,0,1,1,1,2,2,2,
-    3,3,3,4,4,4,5,5,5,3,3,3,4,4,4,5,5,5,3,3,3,4,4,4,5,5,5,
-    6,6,6,7,7,7,8,8,8,6,6,6,7,7,7,8,8,8,6,6,6,7,7,7,8,8,8
-  ].freeze
-
-  AllDigits = [1, 2, 3, 4, 5, 6, 7, 8, 9].freeze
-
-  BoxToIndex = [0, 3, 6, 27,30, 33, 54, 57, 60].freeze
 
   class Puzzle
     ASCII = ".123456789"
@@ -62,6 +47,12 @@ module Sudoku
       @grid[row*9 + col] = newvalue
     end
 
+    BoxOfIndex = [
+      0,0,0,1,1,1,2,2,2,0,0,0,1,1,1,2,2,2,0,0,0,1,1,1,2,2,2,
+      3,3,3,4,4,4,5,5,5,3,3,3,4,4,4,5,5,5,3,3,3,4,4,4,5,5,5,
+      6,6,6,7,7,7,8,8,8,6,6,6,7,7,7,8,8,8,6,6,6,7,7,7,8,8,8
+    ].freeze
+
     def each_unknown
       0.upto 8 do |row|
         0.upto 8 do |col|
@@ -71,91 +62,103 @@ module Sudoku
           yield row, col, box
         end
       end
+    end
 
-      def has_duplicates?
-        0.upto(8) {|row| return true if rowdigits(row).uniq! }
-        0.upto(8) {|col| return true if coldigits(col).uniq! }
-        0.upto(8) {|box| return true if boxdigits(box).uniq! }
+    def has_duplicates?
+      0.upto(8) {|row| return true if rowdigits(row).uniq! }
+      0.upto(8) {|col| return true if coldigits(col).uniq! }
+      0.upto(8) {|box| return true if boxdigits(box).uniq! }
 
-        false
-      end
+      false
+    end
 
-      def possible(row, col, box)
-        AllDigits - (rowdigits(row) + coldigits(col) + boxdigits(box))
-      end
+    AllDigits = [1, 2, 3, 4, 5, 6, 7, 8, 9].freeze
 
-      private
+    def possible(row, col, box)
+      AllDigits - (rowdigits(row) + coldigits(col) + boxdigits(box))
+    end
 
-      def rowdigits(row)
-        @grid[row*9,9] - [0]
-      end
+    private
 
-      def coldigits(col)
-        result = []
-        col.step(80,9) {|i|
-          v = @grid[i]
-          result << v if (v != 0)
-        }
-        result
-      end
+    def rowdigits(row)
+      @grid[row*9,9] - [0]
+    end
 
-      def boxdigits(b)
-        i = BoxToIndex[b]
+    def coldigits(col)
+      result = []
+      col.step(80,9) {|i|
+        v = @grid[i]
+        result << v if (v != 0)
+      }
+      result
+    end
 
-        [
-          @grid[i], @grid[i+1], @grid[i+2],
-          @grid[i+9], @grid[i+10], @grid[i+11],
-          @grid[i+18], @grid[i+19], @grid[i+20]
-        ] - [0]
-      end
+    BoxToIndex = [0, 3, 6, 27,30, 33, 54, 57, 60].freeze
 
-      def Sudoku.scan(puzzle)
-        unchanged = false
+    def boxdigits(b)
+      i = BoxToIndex[b]
 
-        until unchanged
-          unchanged = true
-          rmin,cmin,pmin = nil
-          min = 10
+      [
+        @grid[i], @grid[i+1], @grid[i+2],
+        @grid[i+9], @grid[i+10], @grid[i+11],
+        @grid[i+18], @grid[i+19], @grid[i+20]
+      ] - [0]
+    end
+  end
 
-          puzzle.each_unknown do |row, col, box|
-            p = puzzle.possible(row, col, box)
+  class Invalid < StandardError
+  end
 
-            case p.size
-            when 0
-              raise Impossible
-            when 1
-              puzzle[row,col] = p[0]
-              unchanged = false
-            else
-              if unchanged && p.size < min
-                min = p.size
-                rmin, cmin, pmin = row, col, p
-              end
-            end
+  class Impossible < StandardError
+  end
+
+
+
+  def Sudoku.scan(puzzle)
+    unchanged = false
+
+    until unchanged
+      unchanged = true
+      rmin,cmin,pmin = nil
+      min = 10
+
+      puzzle.each_unknown do |row, col, box|
+        p = puzzle.possible(row, col, box)
+
+        case p.size
+        when 0
+          raise Impossible
+        when 1
+          puzzle[row,col] = p[0]
+          unchanged = false
+        else
+          if unchanged && p.size < min
+            min = p.size
+            rmin, cmin, pmin = row, col, p
           end
         end
-
-        return rmin, cmin, pmin
-      end
-
-      def Sudoku.solve(puzzle)
-        puzzle = puzzle.dup
-
-        r,c,p = scan(puzzle)
-        return puzzle if r == nil
-
-        p.each do |guess|
-          puzzle[r,c] = guess
-
-          begin
-            return solve(puzzle)
-          rescue Impossible
-            next
-          end
-        end
-
-        raise Impossible
       end
     end
+
+    return rmin, cmin, pmin
+  end
+
+  def Sudoku.solve(puzzle)
+    puzzle = puzzle.dup
+
+    r,c,p = scan(puzzle)
+    return puzzle if r == nil
+
+    p.each do |guess|
+      puzzle[r,c] = guess
+
+      begin
+        return solve(puzzle)
+      rescue Impossible
+        next
+      end
+    end
+
+    raise Impossible
   end
 end
